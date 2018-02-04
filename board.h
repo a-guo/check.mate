5328a368f8de31794ece44d2102a402acffcefbe
Addie Guo <addieguo98@gmail.com>
2018-02-04 16:17:05 -0500

    Update board.h

--- board.h
+++ board.h
@@ -2,17 +2,48 @@
 #define BOARD_H
 
 
-#include <vector>
-#include <string>
+#include <iostream>
+#include "trash.h"
+#include "textdisplay.h"
+#include "window.h"
 
 class Piece;
+class Player;
 
 class Board {
-    std::vector<std::vector<string>> theBoard;
-    std::vector<Piece> pieces;
+    int maxPlayer;
+    int numPlayer;
+    int currentPlayer;
+    int* scores;
+    Piece*** theBoard;
+    bool** placed;
+    Textdisplay* theDisplay;
+    Player** players;
+    Trash* theTrash;
 public:
-    Board();
+    Board(int maxPlayer);
+    // default initialize to 2 players
+    void defaultInit(Player* p1, Player* p2);
+    void notify(int iRow, int iCol, int fRow, int fCol);
+    void addPlayer(Player* p);
+    void setPiece(char type, int row, int col);
+    Piece* getPiece(int row, int col);
+    void delPiece(int row, int col);
+    bool isPlaced(int row, int col);
+    void setPlaced(int row, int col, bool value);
+    void notifyPlayer(int row, int col);
+    Player* getPlayer();
+    int getTurn();
+    bool checkForStart();
+    void nextTurn();
+    void setTurn(int tNum);
+    void addScore();
+    void printScore();
+    
     ~Board();
+
+    friend std::ostream &operator<<(std::ostream &out, const Board &b);
+
 };
 
 #endif
