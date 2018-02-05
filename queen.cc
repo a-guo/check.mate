8e097b9a850b9951e3b8a19aa6aac790c8ba752d
Addie Guo <addieguo98@gmail.com>
2018-02-04 23:45:10 -0500

    Update queen.cc

--- queen.cc
+++ queen.cc
@@ -1,13 +1,18 @@
 #include "queen.h"
+#include "board.h"
+#include "textdisplay.h"
+#include "player.h"
+#include <cstdlib>
 
 using namespace std;
 
-Queen::Queen(int x, int y) : x(x), y(y) {};
+Queen::Queen(char type, int row, int col, Player* p, Board* theBoard)
+            : Piece(type, row, col, p, theBoard) {}
 
-Queen::~Queen() {
-  return;
-}
+bool Queen::validMove(int row, int col);
+bool Queen::tryNextMove();
+void Queen::check(Player* opp);
+void Queen::uncheck(Player* opp, int row, int col);
 
-void Queen::setPiece(int x, int y);
-
-std::vector Queen::getPiece(Piece piece);
+void Queen::notifyDisplay(Textdisplay& t) {}
+void Queen::upgrade() {}
