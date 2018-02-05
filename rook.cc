100d3c45b14373f386b6cf390d70a3619e369fda
Addie Guo <addieguo98@gmail.com>
2018-02-04 22:52:06 -0500

    Update rook.cc

--- rook.cc
+++ rook.cc
@@ -1,13 +1,44 @@
 #include "rook.h"
+#include "board.h"
+#include "textdisplay.h"
+#include "player.h"
+#include <cstdlib>
 
 using namespace std;
 
-Rook::Rook(int x, int y) : x(x), y(y) {};
+Rook::Rook(char type, int row, int col, Player* p, Board* theBoard)
+          : Piece(type, row, col, p, theBoard), moved(false) {}
 
-Rook::~Rook() {
-  return;
+bool Rook::validMove(int row, int col) {
+
 }
 
-void Rook::setPiece(int x, int y);
+bool Rook::tryNextMove() {
+  for (int j = col + 1; j < GRID_SIZE; j++) {
+    if (validMove(row, j)) return true;
+  }
 
-std::vector Rook::getPiece(Piece piece);
+  for (int j = 0; j < col; j++) {
+    if (validMove(row, j)) return true;
+  }
+
+  for (int i = row + 1; i < GRID_SIZE; i++) {
+    if (validMove(i, col)) return true;
+  }
+
+  for (int i = 0; i < row; i++) {
+    if (validMove(i, col)) return true;
+  }
+  return false;
+}
+
+void Rook::check(Player* opp) {
+
+}
+
+void Rook::uncheck(Player* opp, int row, int col) {
+
+}
+
+void Rook::notifyDisplay(Textdisplay& t) {}
+void Rook::upgrade() {}
