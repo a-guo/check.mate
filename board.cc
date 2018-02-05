739d7ebf2271caafc92855feaf8f28b3fe5733d9
Addie Guo <addieguo98@gmail.com>
2018-02-04 22:31:03 -0500

    Update board

--- board.cc
+++ board.cc
@@ -2,8 +2,35 @@
 
 using namespace std;
 
-// allocate memory
-Board();
+int maxPlayer;
+int numPlayer;
+int currentPlayer;
+int* scores;
+Piece*** theBoard;
+bool** placed;
+Textdisplay* theDisplay;
+Player** players;
+Trash* theTrash; 
 
-// deallocate memory
-~Board();
+Board::Board(int maxPlayer);
+// default initialize to 2 players
+void Board::defaultInit(Player* p1, Player* p2);
+void Board::notify(int iRow, int iCol, int fRow, int fCol);
+void Board::addPlayer(Player* p);
+void Board::setPiece(char type, int row, int col);
+Piece* Board::getPiece(int row, int col);
+void Board::delPiece(int row, int col);
+bool Board::isPlaced(int row, int col);
+void Board::setPlaced(int row, int col, bool value);
+void Board::notifyPlayer(int row, int col);
+Player* Board::getPlayer();
+int Board::getTurn();
+bool Board::checkForStart();
+void Board::nextTurn();
+void Board::setTurn(int tNum);
+void Board::addScore();
+void Board::printScore();
+
+Board::~Board();
+
+ostream &Board::operator<<(ostream &out, const Board &b);
