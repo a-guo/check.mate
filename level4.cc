5bc70732a66a98b22a2fcedb08abcc19f9678998
Addie Guo <addieguo98@gmail.com>
2018-02-06 17:02:58 -0500

    Update level4.cc

--- level4.cc
+++ level4.cc
@@ -1,4 +1,4 @@
-#include "level3.h"
+#include "level4.h"
 #include "player.h"
 #include "piece.h"
 #include <math.h>
@@ -7,10 +7,9 @@
 
 using namespace std;
 
-Level3::Level3(Board* theBoard, int numOpp, int bottomLine, int playerNum)
+Level4::Level4(Board* theBoard, int numOpp, int bottomLine, int playerNum)
               : Engine(theBoard, numOpp, bottomLine, playerNum) {}
 
-bool Level3::move() {
-
+bool Level4::move() {
 
 }
