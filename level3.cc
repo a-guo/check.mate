e61925dd1532ffc21784a89ff1b654b64d898612
Addie Guo <addieguo98@gmail.com>
2018-02-05 20:57:39 -0500

    Create level3.cc

---
+++ level3.cc
@@ -0,0 +1,15 @@
+#include "level3.h"
+#include "player.h"
+#include "piece.h"
+#include <math.h>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+Level3::Level3(Board* theBoard, int numOpp, int bottomLine, int playerNum)
+              : Engine(theBoard, numOpp, bottomLine, playerNum) {}
+
+bool Level3::move() {
+
+}
