4f625f5036a830ccaf78a925bc11cfbff4b8e3ad
Addie Guo <addieguo98@gmail.com>
2018-02-05 20:04:16 -0500

    Update engine.cc

--- engine.cc
+++ engine.cc
@@ -6,6 +6,35 @@
 
 using namespace std;
 
-Engine::Engine(Board* theBoard, int numOpp, int bottomLine, int playerNum);
-void Engine::addPiece(Piece *p, int row, int col);
-void Engine::removePiece(int row, int col);
+Engine::Engine(Board* theBoard, int numOpp, int bottomLine, int playerNum)
+              : Player(theBoard, numOpp, bottomLine, playerNum) {
+  pList = new Piece* [GRID_SIZE * GRID_SIZE];
+  values = new int* [GRID_SIZE];
+  for(int i = 0; i < GRID_SIZE; i++) {
+    values[i] = new int [GRID_SIZE];
+    for(int j = 0; j < GRID_SIZE; j++) {
+      values[i][j] = 0;
+    }
+  }
+}
+
+void Engine::addPiece(Piece *p, int row, int col) {
+
+}
+void Engine::removePiece(int row, int col) {
+  if (myPiece[row][col] != NULL) {
+    for(int i = 0; i < numPiece; i++) {
+      if (pList[i] == myPiece[row][col]) {
+        pList[i] = NULL;
+        for(int j = i; j < numPiece; j++) {
+          pList[j] = pList[j + 1];
+        }
+        numPiece--;
+        break;
+      }
+    }
+    values[row][col] = 0;
+    myPiece[row][col]->notifyNeighboursMoved();
+    myPiece[row][col] = NULL;
+  }
+}
