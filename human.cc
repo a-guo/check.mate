60c85b8c64f238c410ac5a24789563e5093edb46
Addie Guo <addieguo98@gmail.com>
2018-02-05 19:45:15 -0500

    Update human.cc

--- human.cc
+++ human.cc
@@ -6,7 +6,41 @@
 
 using namespace std;
 
-Human::Human(Board* theBoard, int numOpp, int bottomLine, int playerNum);;
-void Human::addPiece(Piece *p, int row, int col);
-void Human::removePiece(int row, int col);
-bool Human::move();
+Human::Human(Board* theBoard, int numOpp, int bottomLine, int playerNum)
+            : Player(theBoard, numOpp, bottomLine, playerNum) {}
+
+void Human::addPiece(Piece *p, int row, int col) {
+
+}
+
+void Human::removePiece(int row, int col) {
+  if (myPiece[row][col] != NULL) {
+    myPiece[row][col]->notifyNeighboursMoved();
+    myPiece[row][col] = NULL;
+    values[row][col] = 0;
+  }
+}
+
+bool Human::move() {
+  char iColC;
+  int iRow;
+  char fColC;
+  int fRow;
+
+  cin >> iColC >> iRow;
+  cin >> fColC >> fRow;
+
+  int iCol = iColC - 'a';
+  int fCol = fColC - 'a';
+
+  iRow = GRID_SIZE - iRow;
+  fRow = GRID_SIZE - fRow;
+
+  if(myPiece[iRow][iCol] == NULL) return false;
+  if(myPiece[iRow][iCol]->move(fRow, fCol)) {
+    myPiece[fRow][fCol] = myPiece[iRow][iCol];
+    myPiece[iRow][iCol] = NULL;
+    theBoard->notify(iRow, iCol, fRow, fCol);
+    return true;
+  }
+}
