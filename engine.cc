69ffecddec44d1a89604172ee60df189d027efcf
Addie Guo <addieguo98@gmail.com>
2018-02-08 23:30:24 -0500

    Update addPiece

--- engine.cc
+++ engine.cc
@@ -19,8 +19,62 @@ Engine::Engine(Board* theBoard, int numOpp, int bottomLine, int playerNum)
 }
 
 void Engine::addPiece(Piece *p, int row, int col) {
-
+  myPiece[row][col] = p;
+  pList[numPiece] = p;
+  numPiece++;
+  if (p->getType() == 'K' || p->getType() == 'k') {
+    theKing = p;
+  }
+  char type = p->getType();
+  // negative == white; positive == black
+  switch(type) {
+  case 'P':
+      values[row][col] = 1;
+      break;
+  case 'p':
+      values[row][col] = 1;
+      break;
+  case 'N':
+      values[row][col] = 2;
+      break;
+  case 'n':
+      values[row][col] = 2;
+      break;
+  case 'B':
+      values[row][col] = 3;
+      break;
+  case 'b':
+      values[row][col] = 3;
+      break;
+  case 'R':
+      values[row][col] = 3;
+      break;
+  case 'r':
+      values[row][col] = 3;
+      break;
+  case 'Q':
+      values[row][col] = 5;
+      break;
+  case 'q':
+      values[row][col] = 5;
+      break;
+  case 'K':
+      values[row][col] = 10;
+      break;
+  case 'k':
+      values[row][col] = 10;
+      break;
+  default:
+      break;
+  }
+  // check opponents
+  for (int i = 0; i < numOpp; i++) {
+    if (i != this->playerNum) {
+      myPiece[row][col]->check(Opps[i]);
+    }
+  }
 }
+
 void Engine::removePiece(int row, int col) {
   if (myPiece[row][col] != NULL) {
     for(int i = 0; i < numPiece; i++) {
