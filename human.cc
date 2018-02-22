9e2a72b52ce2f29c18b82760a6e4ec582e42efc1
Addie Guo <addieguo98@gmail.com>
2018-02-21 20:08:10 -0500

    Update adding pieces

--- human.cc
+++ human.cc
@@ -10,7 +10,59 @@ Human::Human(Board* theBoard, int numOpp, int bottomLine, int playerNum)
             : Player(theBoard, numOpp, bottomLine, playerNum) {}
 
 void Human::addPiece(Piece *p, int row, int col) {
+  myPiece[row][col] = p;
 
+  if (p->getType() == 'K' || p->getType() == 'k') {
+    theKing = p;
+  }
+  char type = p->getType();
+  // negative = white; positive = black
+  switch(type)
+  {
+  case 'P':
+    values[row][col] = 1;
+    break;
+  case 'p':
+    values[row][col] = 1;
+    break;
+  case 'N':
+    values[row][col] = 2;
+    break;
+  case 'n':
+    values[row][col] = 2;
+    break;
+  case 'B':
+    values[row][col] = 3;
+    break;
+  case 'b':
+    values[row][col] = 3;
+    break;
+  case 'R':
+    values[row][col] = 3;
+    break;
+  case 'r':
+    values[row][col] = 3;
+    break;
+  case 'Q':
+    values[row][col] = 5;
+    break;
+  case 'q':
+    values[row][col] = 5;
+    break;
+  case 'K':
+    values[row][col] = 10;
+    break;
+  case 'k':
+    values[row][col] = 10;
+  default:
+    break;
+  }
+  // now have to check opponents
+  for (int i = 0; i < numOpp; i++) {
+    if (i != this->playerNum) {
+      myPiece[row][col]->check(Opps[i]);
+    }
+  }
 }
 
 void Human::removePiece(int row, int col) {
