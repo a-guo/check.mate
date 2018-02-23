bb0d39c5927a9a79deff2fd2a92ef0903d87d637
Addie Guo <addieguo98@gmail.com>
2018-02-22 23:24:43 -0500

    Update next move check

--- knight.cc
+++ knight.cc
@@ -1,13 +1,27 @@
 #include "knight.h"
+#include <cstdlib>
 
 using namespace std;
 
-Knight::Knight (int x, int y) : x(x), y(y) {};
+Knight::Knight(char type, int row, int col, Player* p, Board* theBoard) :
+              Piece(type, row, col, p, theBoard) {}
 
-Knight::~Knight() {
-  return;
+bool Knight::validMove(int row, int col);
+
+bool Knight::tryNextMove() {
+  return (validMove(row + 2, col + 1) ||
+          validMove(row - 2, col + 1) ||
+          validMove(row + 2, col - 1) ||
+          validMove(row - 2, col - 1) ||
+          validMove(row + 1, col + 2) ||
+          validMove(row + 1, col - 2) ||
+          validMove(row - 1, col + 2) ||
+          validMove(row - 1, col - 2));
 }
 
-void Knight::setPiece(int x, int y);
+void Knight::check(Player* opp);
 
-std::vector Knight::getPiece(Piece piece);
+void Knight::uncheck(Player* opp, int row, int col);
+
+void Knight::notifyDisplay(Textdisplay& t) {}
+void Knight::upgrade() {}
