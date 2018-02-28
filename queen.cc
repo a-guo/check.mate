0de7130ee81c3e7efe7605b94bad2aefb1112809
Addie Guo <addieguo98@gmail.com>
2018-02-28 12:00:24 -0500

    update move logic

--- queen.cc
+++ queen.cc
@@ -9,7 +9,23 @@ using namespace std;
 Queen::Queen(char type, int row, int col, Player* p, Board* theBoard)
             : Piece(type, row, col, p, theBoard) {}
 
-bool Queen::validMove(int row, int col);
+bool Queen::validMove(int row, int col) {
+  if ((this->col == col && this->row == row) || (col + 1 > GRID_SIZE) || col < 0) {
+    return false;
+  }
+  if ((this->col == col && this->row == row) || (row + 1 > GRID_SIZE) || row < 0) {
+    return false;
+  }
+  // moving like a rook
+  // R--->
+  if (row > this->row && this->col == col) {
+    for (int i = this->row + 1; i < row - 1; i++) {
+      if (theBoard->isPlaced(i, this->col)) {
+        return false;
+      }
+    }
+  }
+}
 bool Queen::tryNextMove();
 void Queen::check(Player* opp);
 void Queen::uncheck(Player* opp, int row, int col);
