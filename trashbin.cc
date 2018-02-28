19246c63976ee7fee2ba9805efc371851328bc33
Addie Guo <addieguo98@gmail.com>
2018-02-28 11:53:31 -0500

    updated functions

--- trashbin.cc
+++ trashbin.cc
@@ -1,7 +1,36 @@
 #include "trashbin.h"
 #include <stdlib.h>
 
-TrashBin(int capacity);
-void dump(Piece* garbage);
-void emptyBin();
-Piece* recover(Piece* p);
+TrashBin::TrashBin(int capacity) : capacity{capacity}, numTrash{0} {
+  theBin = new Piece* [capacity];
+}
+
+void TrashBin::dump(Piece* garbage) {
+  if (numTrash + 1 >= capacity) {
+    emptyBin();
+  }
+  theBin[numTrash] = garbage;
+  numTrash++;
+}
+
+void TrashBin::emptyBin() {
+  for (int i = 0; i < numTrash; i++) {
+    delete theBin[i];
+  }
+  numTrash = 0;
+}
+
+Piece* TrashBin::recover(Piece* p) {
+  for (int i = 0; i < numTrash; i++) {
+    if (theBin[i] == p) {
+      theBin[i] = NULL;
+      numTrash--;
+      for (int j = i; j < numTrash; j++) {
+        theBin[j] = theBin[j + 1];
+      }
+      return p;
+    }
+  }
+  // nothing if found
+  return NULL;
+}
