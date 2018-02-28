8e2ea4bbeec0bf7a81c9fbb6be3896e389b70879
Addie Guo <addieguo98@gmail.com>
2018-02-28 14:09:14 -0500

    update moves

--- queen.cc
+++ queen.cc
@@ -20,12 +20,35 @@ bool Queen::validMove(int row, int col) {
   // R--->
   if (row > this->row && this->col == col) {
     for (int i = this->row + 1; i < row - 1; i++) {
-      if (theBoard->isPlaced(i, this->col)) {
-        return false;
-      }
+      if (theBoard->isPlaced(i, this->col)) return false;
     }
   }
+  // up
+  else if (row < this->row && this->col == col) {
+    for (int i = this->row - 1; i > row - 1; i--) {
+      if (theBoard->isPlaced(i, this->col)) return false;
+    }
+  }
+  // R up
+  else if (col > this->col && this->row == row) {
+    for (int i = this->col + 1; i < col - 1; i++) {
+      if (theBoard->isPlaced(this->row, i)) return false;
+    }
+  }
+  // R down
+  else if (col < this->col && this->row == row) {
+    for (int i = this->col - 1; i > col - 1; i--) {
+      if (theBoard->isPlaced(this->row, i)) return false;
+    }
+  }
+  else if (row > this->row && col > this->col) {
+    for (int i = 1; i < row - this->row; i++) {
+      if (theBoard->isPlaced(this->row + 1, this->col + i)) return false;
+    }
+  }
+  // up + left
 }
+
 bool Queen::tryNextMove();
 void Queen::check(Player* opp);
 void Queen::uncheck(Player* opp, int row, int col);
