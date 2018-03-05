80e6a0419818305bdd9ed00c90b6a6c25fa6e144
Addie Guo <addieguo98@gmail.com>
2018-03-05 11:13:01 -0500

    attack opponent

--- level2.cc
+++ level2.cc
@@ -11,5 +11,24 @@ Level2::Level2(Board* theBoard, int numOpp, int bottomLine, int playerNum)
               : Engine(theBoard, numOpp, bottomLine, playerNum) {}
 
 bool Level2::move() {
-
+  // trying to attack
+  for (int fRow = 0; fRow < GRID_SIZE; fRow++) {
+    for (int fCol = 0; fCol < GRID_SIZE; fCol++) {
+      // looking for target to attack
+      if (abs(Opps[0]->getScore(fRow, fCol)) > 0 && Opps[0]->isChecked(fRow, fCol)) {
+        for (pList[i]->move(fRow, fCol)) {
+          int iCol = pList[i]->getCol();
+          int iRow = pList[i]->getRow();
+          myPiece[fRow][fCol] = myPiece[iRow][iCol];
+          myPiece[iRow][iCol] = NULL;
+          cout << "Computer moves " << (char)('a' + iCol) << GRID_SIZE - iRow
+          << " " << (char)('a' + fCol) << GRID_SIZE - fRow << endl;
+          theBoard->notify(iRow, iCol, fRow, fCol);
+          return true;
+        }
+      }
+    }
+  }
+  
+  // no attack available, randomly move
 }
