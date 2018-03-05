288813bea7ba117b2d1bbeb2077b20e2080da06b
Addie Guo <addieguo98@gmail.com>
2018-03-05 09:04:20 -0500

    chess engine logic

--- level1.cc
+++ level1.cc
@@ -14,5 +14,25 @@ bool Level1::move() {
   // randomly select a Piece
   bool done = false;
   int i = rand() % numPiece;
-  while ()
+  while (!pList[i]->tryToMove()) {
+    i = (i + 1) % numPiece;
+  }
+  int iRow = pList[i]->getRow();
+  int iCol = pList[i]->getCol();
+
+  for (int fRow = 0; fRow < GRID_SIZE; fRow++) {
+    for (int fCol = 0; fCol < GRID_SIZE; fCol++) {
+      if (myPiece[iRow][iCol]->move(fRow, fCol)) {
+        theBoard->notify(iRow, iCol, fRow, fCol);
+        myPiece[fRow][fCol] = myPiece[iRow][iCol];
+        myPiece[iRow][iCol] = NULL;
+
+        cout << "Computer moves " << (char)('a' + iCol) << GRID_SIZE - iRow
+        << " " << (char)('a' + fCol) << GRID_SIZE - fRow << endl;
+
+        return true;
+      }
+    }
+  }
+  return true;
 }
