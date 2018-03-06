18cbd5b08d2c2934a30a83f893949d15ec5dfe56
Addie Guo <addieguo98@gmail.com>
2018-03-05 22:40:34 -0500

    defense

--- level3.cc
+++ level3.cc
@@ -11,6 +11,13 @@ Level3::Level3(Board* theBoard, int numOpp, int bottomLine, int playerNum)
               : Engine(theBoard, numOpp, bottomLine, playerNum) {}
 
 bool Level3::move() {
-
-  
+  // defense
+  for (int iRow = 0; iRow < GRID_SIZE; iRow++) {
+    for (int iCol = 0; iCol < GRID_SIZE; iCol++) {
+      if (getScore(iRow, iCol) > 0 && isChecked(iRow, iCol)) {
+        // attack when being attacked
+        
+      }
+    }
+  }
 }
