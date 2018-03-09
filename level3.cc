d584406cfe00a55e4f8e78c705dba69c0a86a2c0
Addie Guo <addieguo98@gmail.com>
2018-03-08 20:16:16 -0500

    attack when attacked

--- level3.cc
+++ level3.cc
@@ -16,6 +16,24 @@ bool Level3::move() {
     for (int iCol = 0; iCol < GRID_SIZE; iCol++) {
       if (getScore(iRow, iCol) > 0 && isChecked(iRow, iCol)) {
         // attack when being attacked
+        for (int fRow = 0; fRow < GRID_SIZE; fRow++) {
+          for (int fCol = 0; fCol < GRID_SIZE; fCol++) {
+            // if target & can attack, then attack
+            if (abs(Opps[0]->getScore(fRow, fCol)) > 0 && Opps[0]->isChecked(fRow, fCol)) {
+              for (int i = 0; i < numPiece; i++ ) {
+                if (pList[i]->move(fRow, fCol)) {
+                  myPiece[fRow][fCol] = myPiece[iRow][iCol];
+                  myPiece[iRow][iCol] = NULL;
+                  cout << "Computer moves " << (char)('a' + iCol) <<
+                  GRID_SIZE - iRow << " " << (char)('a' + fCol) << GRID_SIZE - fRow << endl;
+                  theBoard->notify(iRow, iCol, fRow, fCol);
+                  return true;
+                }
+              }
+            }
+          }
+        }
+        // if no attack, then random
       }
     }
   }
